4224788b0d507895d7879cfd3882d0102eb6d8fd
deshigramofficial <deshigramofficial@gmail.com>
2026-08-23 16:31:08 +0530

    Remove old website files

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='deshigram-v1';
-const CORE=['./','./index.html','./products.html','./about.html','./contact.html','./css/style.css','./images/logo.webp'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request)));
-});
